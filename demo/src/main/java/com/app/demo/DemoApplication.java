32edfa685c19fede8a5297f114a19808e64516f3
wangjiangpeng <wangjiangpeng01@baidu.com>
2017-03-31 17:34:38 +0800

    整理

--- demo/src/main/java/com/app/demo/DemoApplication.java
+++ demo/src/main/java/com/app/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.app.demo;
 
+import android.os.Environment;
+
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import base.library.BaseApplication;
 import base.library.MLog;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 /**
  * Created by wangjiangpeng01 on 2016/11/23.
@@ -11,9 +21,8 @@ public class DemoApplication extends BaseApplication {
     @Override
     public void envInit() {
         try {
-            MLog.e("WJP", "envInit");
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
